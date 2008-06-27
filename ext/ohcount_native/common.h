bbea41e6bfa5ff11ea5ffe165bab8bd107d215db
mitchell <mitchell@frost.(none)>
2008-06-26 20:03:38 -0400

    Include stdlib to remove language_breakdown.c warnings.

--- ext/ohcount_native/common.h
+++ ext/ohcount_native/common.h
@@ -20,6 +20,7 @@
 /*******************************************
  Common Headers
 *******************************************/
+#include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
