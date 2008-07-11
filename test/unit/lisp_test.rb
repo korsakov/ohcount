1c47b1837f2f7ada6a7a200cf95ead52f7b976bd
Fedor Korsakov <fedorkorsakov@fedor-korsakovs-computer.local>
2008-07-11 04:01:08 -0500

    Additional test cases for Lisp.

--- test/unit/lisp_test.rb
+++ test/unit/lisp_test.rb
@@ -17,6 +17,16 @@ class Ohcount::LispTest < Ohcount::Test
 		assert_equal lb, Ohcount::parse(' """"""', "lisp")
 	end
 
+	def test_empty_string
+		lb = [Ohcount::LanguageBreakdown.new("lisp", '""', "", 0)]
+		assert_equal lb, Ohcount::parse(' ""', "lisp")
+	end
+
+	def test_char_string
+		lb = [Ohcount::LanguageBreakdown.new("lisp", '"a"', "", 0)]
+		assert_equal lb, Ohcount::parse(' "a"', "lisp")
+	end
+
 	def test_comprehensive
 		verify_parse("lsp1.lsp")
 	end
