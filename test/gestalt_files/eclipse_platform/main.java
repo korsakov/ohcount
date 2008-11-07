042be486589d90b7b3b6e1e9c12393216deac4b7
Jason Allen <jason@Macintosh-6.local>
2008-11-07 13:42:47 -0800

    [FIX] missed adding files to prev checkin

---
+++ test/gestalt_files/eclipse_platform/main.java
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * Gibberish Code Copyrighted Just For The Hell Of It.
+ ******************************************************************************/
+ package net.ohloh.www;
+
+ import java.text.SimpleDateFormat;
+ import java.util.Date;
+ import java.util.HashMap;
+ import java.util.Map;
+ import org.eclipse.core.lotsa_stuff;
+
+ public class AptPlugin extends Plugin {
+	   public static final String PLUGIN_ID = "org.eclipse.jdt.apt.core"; //$NON-NLS-1$
+ }
