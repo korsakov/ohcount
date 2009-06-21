f349ffa82ad3bf6ab350e7a171f7dcf2d46146e5
Andy Verprauskus <andy@ohloh.net>
2009-06-21 09:03:52 -0700

    [CHANGE] complete last commit, add second necessary test

--- test/unit/ruby/source_file_list_test.rb
+++ test/unit/ruby/source_file_list_test.rb
@@ -2,6 +2,16 @@ require 'test/unit'
 require '../../../ruby/gestalt'
 
 class SourceFileListTest < Test::Unit::TestCase
-	paths = [File.dirname(__FILE__)]
-	assert Ohcount::SourceFileList.new(:paths => paths).size > 0
+
+	def test_source_file_list_initializes
+		paths = [File.dirname(__FILE__)]
+		assert Ohcount::SourceFileList.new(:paths => paths).size > 0
+	end
+
+	def test_source_file_filenames
+		filenames = ["x", "y", "z"]
+		sf = Ohcount::SourceFile.new("foo", :contents => "bar", :filenames => filenames)
+		assert_equal filenames, sf.filenames
+	end
 end
+
