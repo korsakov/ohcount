1a00414ef0c0338ac741ebebe3367599c4dc513c
andy <andy@andy306.(none)>
2009-07-14 02:44:05 -0700

    [FIX] allow gestalts tests to be run by individually

--- test/unit/ruby/gestalt/gestalt_test.rb
+++ test/unit/ruby/gestalt/gestalt_test.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require File.dirname(__FILE__) + '/../test_helper.rb'
 require File.dirname(__FILE__) + '/definitions_test'
 require File.dirname(__FILE__) + '/file_rule_test'
 require File.dirname(__FILE__) + '/filename_rule_test'
