50e8a86d8e3bd885f759897ceb5d9ea7b278dcf0
Robin Luckey <rluckey@blackducksoftware.com>
2011-12-15 14:48:16 -0800

    Corrections to Logtalk unit tests

--- test/unit/parsers/test_logtalk.h
+++ test/unit/parsers/test_logtalk.h
@@ -2,14 +2,14 @@
 void test_logtalk_comments() {
   test_parser_verify_parse(
     test_parser_sourcefile("logtalk", " %comment"),
-    "logtalk", "", "//comment", 0
+    "logtalk", "", "%comment", 0
   );
 }
 
 void test_logtalk_empty_comments() {
   test_parser_verify_parse(
     test_parser_sourcefile("logtalk", " %\n"),
-    "logtalk", "", "//\n", 0
+    "logtalk", "", "%\n", 0
   );
 }
 
