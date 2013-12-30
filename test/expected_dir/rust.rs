d40e04bdaa41e0f782cb5e82e489de45ca9b6e77
Abhay Mujumdar <amujumdar@blackducksoftware.com>
2013-12-30 10:37:11 -0500

    Removes whitespace from expected output file

--- test/expected_dir/rust.rs
+++ test/expected_dir/rust.rs
@@ -5,10 +5,10 @@ rust	comment	// Line comments work too
 rust	code	fn main() {
 rust	code	    let nums = [1, 2];
 rust	code	    let noms = ["Tim", "Eston", "Aaron", "Ben"];
-rust	blank	 
+rust	blank	
 rust	code	    let mut odds = nums.iter().map(|&x| x * 2 - 1);
-rust	blank	 
-rust	code   	    for num in odds {
+rust	blank	
+rust	code	    for num in odds {
 rust	code	        do spawn {
 rust	code	            println!("{:s} says hello from a lightweight thread!", noms[num]);
 rust	code	        }
